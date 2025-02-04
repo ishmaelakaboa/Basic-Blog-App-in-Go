7a3ac742041f06ce76af074fb36019839dd3ba24
Ishmael Akaboa <ishmaelakaboa@gmail.com>
2025-02-04 15:00:40 +0000

    Create In-Memory Storage for Posts

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
+import "sync"
 
-type Post struct{
-	ID       int
-	Title    string
-	Content  string
+type Post struct {
+	ID      int
+	Title   string
+	Content string
 }
+
+var (
+	posts   = []Post{}
+	nextID  = 1
+	postMux sync.Mutex
+)
